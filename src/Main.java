af009eea1f5ae01312e41ec95060e0903dfaa915
thiramoss <trs.tiba@gmail.com>
2024-06-11 23:35:37 -0300

    Primeiro commmit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("La La Land");
+        meuFilme.setAnoDeLancamento(2016);
+        meuFilme.setDuracaoEmMinutos(128);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.getSomaDasAvaliacoes());
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
